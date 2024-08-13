50efc0ab301a11d9a83e6ad421b693f8c3c26e2a
Nitin1600 <nitgpatil1@gmail.com>
2024-08-14 00:20:11 +0530

    Files for 13th august 2024

---
+++ src/ex_13082024/Lab001.py
@@ -0,0 +1,52 @@
+result = max(3,4)
+print(result)
+
+a = 45666666666666666666666
+print(type(a))
+print(a)
+
+gst = 18.45
+print(type(gst))
+print(gst)
+
+a = pow(2, 3)
+print(type(a))
+print(a)
+
+a = max(2, 3)
+print(type(a))
+print(a)
+
+a = min(2, 3)
+print(type(a))
+print(a)
+
+a = abs(-1)
+print(type(a))
+print(a)
+
+are_you_in_usa = False
+print(are_you_in_usa)
+
+name = input("Enter your name: ")
+print("Hi ", name)
+print(type(name))
+
+name = input("Enter your name: ")
+print("Hi ", name)
+print(type(name))
+
+num1 = int(input("Enter the num1: "))
+num2 = int(input("Enter the num2: "))
+
+print("Sum is: ", num1+num2)
+print("Sub is: ", num1-num2)
+print("Mul is: ", num1*num2)
+print("Div is: ", num1/num2, end="\n")
+
+print(3/2)
+
+
+
+
+
---
+++ src/ex_13082024/Lab003.py
@@ -0,0 +1,16 @@
+# name = "Amith"
+# name = name + 1
+# print(name)
+
+first_name = "Prakash "
+last_name = "Patil"
+final_name = first_name + last_name
+print(final_name)
+
+how_many_planes_i_have = None
+print(how_many_planes_i_have)
+
+age = 10
+age2 = 10
+print(id(age))
+print(id(age2))
