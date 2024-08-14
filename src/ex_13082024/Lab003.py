877b2d2895085364e4ee6dede5dabaf2634cfc42
Nitin1600 <nitgpatil1@gmail.com>
2024-08-14 08:23:59 +0530

    Files for 13th august 2024

--- src/ex_13082024/Lab003.py
+++ src/ex_13082024/Lab003.py
@@ -14,3 +14,42 @@ age = 10
 age2 = 10
 print(id(age))
 print(id(age2))
+
+x = input("Type a number:")
+y = input("Type another number:")
+sum = int(x) + int(y)
+print("The sum is:",sum)
+
+print(complex(2.5, -18.2))
+c = complex(2.5, -18.2)
+c = c + 1
+print(c)
+
+print(r'C:\some\name')
+
+random_string = "I am a Batman"
+print(len(random_string))
+
+batman = "Bruce Wayne"
+first = batman[0]
+print(first)
+space = batman[5]
+print(space)
+last = batman[len(batman) -1]
+print(last)
+
+print(batman[-1])
+print(batman[-5])
+
+b = 22
+b[0] = 2
+
+str1 = "Amith"
+print(id(str1))
+str1 = "Bidar"
+print(id(str1))
+
+val = None
+print(val)
+print(type(val))
+
